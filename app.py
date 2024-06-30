e03a945887da58fdd9902bd296b5f11f042ae264
Kauan Dantas <kauan.dantas0507@gmail.com>
2024-06-30 13:45:49 -0300

    projeto scraping

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
@@ -6,27 +7,79 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
+import sqlite3
 
 app = Flask(__name__)
 
+
+def init_db():
+    try:
+        
+        db_path = os.path.join(os.path.dirname(__file__), 'itens.db')
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+
+        
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS itens (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome TEXT NOT NULL,
+                valor TEXT,
+                vendedor TEXT,
+                parcelas_valor TEXT,
+                avaliacao TEXT,
+                cupons_desconto TEXT
+            )
+        ''')
+
+        conn.commit()
+        conn.close()
+    except Exception as e:
+        print(f"Erro ao inicializar o banco de dados: {e}")
+
+
+def calcular_valor_medio(itens):
+    valores = []
+    for item in itens:
+        valor_str = item['Valor'].replace('R$', '').strip()
+        try:
+            valor_float = float(valor_str.replace('.', '').replace(',', '.'))
+            valores.append(valor_float)
+        except ValueError:
+            print(f"Valor inválido encontrado: {valor_str}")
+    if valores:
+        return sum(valores) / len(valores)
+    else:
+        return 0.0
+
+
+init_db()
+
 @app.route('/')
 def homepage():
-    return render_template("index.html", dados=[])
+    return render_template("index.html", dados=[], valor_medio=0)
 
 @app.route('/scrape')
 def scrape_data():
-    chrome_options = Options()
-    chrome_options.add_argument('--headless')  # Execução sem interface gráfica
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=chrome_options)
-
-    url = 'https://lista.mercadolivre.com.br/chuveiro'
-
     try:
+        termo_busca = request.args.get('termo_busca', 'chuveiro')  
+        print(f"chuveiro: {termo_busca}")
+
+        termo_busca = request.args.get('termo_busca', 'bicicleta')
+        print(f"biclicleta: {termo_busca}")
+        
+        chrome_options = Options()
+        chrome_options.add_argument('--headless') 
+        service = Service(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service, options=chrome_options)
+
+        url = f'https://lista.mercadolivre.com.br/{termo_busca}'  
+        print(f"https://lista.mercadolivre.com.br/chuveiro, https://lista.mercadolivre.com.br/bicicleta: {url}")
+
         driver.get(url)
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'ui-search-layout')))
 
-        itens = driver.find_elements(By.CLASS_NAME, 'ui-search-layout__item')[:12]  # Limitar a 12 itens
+        itens = driver.find_elements(By.CLASS_NAME, 'ui-search-layout__item')[:10] #limitar a 10 itens  
 
         dados = []
         for item in itens:
@@ -37,7 +90,7 @@ def scrape_data():
                 nome = ''
 
             try:
-                valor_element = item.find_element(By.CLASS_NAME, 'ui-search-price__part')
+                valor_element = item.find_element(By.CLASS_NAME, 'ui-search-price__second-line')
                 valor = valor_element.text.strip()
             except:
                 valor = ''
@@ -61,7 +114,7 @@ def scrape_data():
                 avaliacao = ''
 
             try:
-                cupom_element = item.find_element(By.CLASS_NAME, 'ui-search-item__discount')
+                cupom_element = item.find_element(By.CLASS_NAME, 'ui-pb-container')
                 cupom = cupom_element.text.strip()
             except:
                 cupom = ''
@@ -75,11 +128,22 @@ def scrape_data():
                 'Cupons de Desconto': cupom
             })
 
-    finally:
         driver.quit()
 
-    return render_template('index.html', dados=dados)
+        
+        valor_medio = calcular_valor_medio(dados)
+
+        return render_template('index.html', dados=dados, valor_medio=valor_medio)
+
+    except Exception as e:
+        print(f"Erro durante a raspagem de dados: {e}")
+        return render_template('error.html', message="Ocorreu um erro durante a raspagem de dados. Por favor, tente novamente mais tarde.")
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
 
